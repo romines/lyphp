97306763bd834bf1807a7853e40c55881df62dfc
Adam Romines <adam.romines@gmail.com>
2017-06-24 15:26:57 -0600

    My solution to 'Exceptional Coding' exercise

--- program.php
+++ program.php
@@ -1,14 +1,20 @@
 <?php
 
 // require_once __DIR__ . '/mymodule.php';
-
+// echo 'program.php . . .';
 array_shift($argv);
-$extant = array_filter($argv, 'file_exists');
+// $extant = array_filter($argv, 'file_exists');
 array_map(function ($filename) {
   // echo ($filename);
-  $file = new SplFileObject($filename);
-  $info = new SplFileInfo($filename);
-  echo $info->getFilename(), "\n";
-}, $extant);
+  try {
+    $file = new SplFileObject($filename);
+    $info = new SplFileInfo($filename);
+    echo $info->getFilename(), "\n";
+  } catch (RuntimeException $e) {
+    echo "Unable to open file at path '", $filename, "'\n";
+  }
+
+}, $argv);
+
 
  ?>
